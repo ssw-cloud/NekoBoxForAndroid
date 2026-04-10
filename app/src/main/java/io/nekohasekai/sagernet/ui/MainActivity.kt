76e03e5400b0567496fc32e04f48b32190e8d116
ssw-cloud <152955975+ssw-cloud@users.noreply.github.com>
2026-04-10 23:54:57 +0800

    Customize links and trim about screen

--- app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
@@ -340,14 +340,9 @@ class MainActivity : ThemedActivity(),
             R.id.nav_traffic -> displayFragment(WebviewFragment())
             R.id.nav_tools -> displayFragment(ToolsFragment())
             R.id.nav_logcat -> displayFragment(LogcatFragment())
-            R.id.nav_faq -> {
-                launchCustomTab("https://matsuridayo.github.io/")
-                return false
-            }
-
             R.id.nav_about -> displayFragment(AboutFragment())
             R.id.nav_tuiguang -> {
-                launchCustomTab("https://neko-box.pages.dev/喵")
+                launchCustomTab("https://ssw.best")
                 return false
             }
 
